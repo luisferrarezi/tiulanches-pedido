2ad63f4eae36ce1ffb0a5cbcc0211d11c19bf527
Luis <luisferrarezi@hotmail.com>
2024-06-08 12:03:43 -0300

    mais ajuste

--- src/main/java/br/com/fiap/tiulanches/core/entity/cliente/Cliente.java
+++ src/main/java/br/com/fiap/tiulanches/core/entity/cliente/Cliente.java
@@ -26,54 +26,54 @@ import lombok.Setter;
 public class Cliente {
 	@Id	
 	@Size(max=11, min=11)
-	@Schema(description = "CPF do cliente sem formatação", example = "70636213005", required = true, maxLength = 11, minLength = 11)
+	@Schema(description = "CPF do cliente sem formatação", example = "70636213005", maxLength = 11, minLength = 11)
 	private String cpf;
 	
 	@Size(max=60)
-	@Schema(description = "Nome do cliente", example = "Luis Antonio", required = true, maxLength = 60)
+	@Schema(description = "Nome do cliente", example = "Luis Antonio", maxLength = 60)
 	private String nome;
 	
 	@Size(max=60)
-	@Schema(description = "Email do cliente", example = "luisantonio@gmail.com", required = true, maxLength = 60)
+	@Schema(description = "Email do cliente", example = "luisantonio@gmail.com", maxLength = 60)
 	private String email;
 
 	@Enumerated(EnumType.ORDINAL)
-	@Schema(implementation = Logado.class, description = "Define se o cliente está logado na aplicação", example = "SIM", required = true)	
+	@Schema(implementation = Logado.class, description = "Define se o cliente está logado na aplicação", example = "SIM")	
 	private Logado logado;
 
 	@NotBlank
 	@Size(max=100)
-	@Schema(description = "Endereço do cliente", example = "Rua Augustina", required = true, maxLength = 100)
+	@Schema(description = "Endereço do cliente", example = "Rua Augustina", maxLength = 100)
 	private String endereco;
 
 	@NotBlank
 	@Size(max=10)
-	@Schema(description = "Número endereço do Cliente", example = "1234", required = true, maxLength = 10)
+	@Schema(description = "Número endereço do Cliente", example = "1234", maxLength = 10)
 	private String numero;
 
 	@NotBlank
 	@Size(max=100)
-	@Schema(description = "Bairro do cliente", example = "Santa Luzia", required = true, maxLength = 100)
+	@Schema(description = "Bairro do cliente", example = "Santa Luzia", maxLength = 100)
 	private String bairro;
 
 	@NotBlank
 	@Size(max=100)
-	@Schema(description = "Cidade do cliente", example = "Potirendaba", required = true, maxLength = 100)
+	@Schema(description = "Cidade do cliente", example = "Potirendaba", maxLength = 100)
 	private String cidade;
 
 	@NotBlank
 	@Size(max=2)
-	@Schema(description = "Estado cidade do cliente", example = "SP", required = true, maxLength = 2)
+	@Schema(description = "Estado cidade do cliente", example = "SP", maxLength = 2)
 	private String estado;
 
 	@NotBlank
 	@Size(max=9)
-	@Schema(description = "CEP do cliente", example = "11111-000", required = true, maxLength = 9)
+	@Schema(description = "CEP do cliente", example = "11111-000", maxLength = 9)
 	private String cep;
 
 	@NotBlank
 	@Size(max=15)
-	@Schema(description = "Telefone do cliente", example = "(11) 99999-9999", required = true, maxLength = 15)
+	@Schema(description = "Telefone do cliente", example = "(11) 99999-9999", maxLength = 15)
 	private String telefone;
 
 	public boolean isLogado(){
